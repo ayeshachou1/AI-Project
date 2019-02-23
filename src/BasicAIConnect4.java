3af711cce8c067727f6eb3ffafe69c01dfc92f2b
Ayesha Chou <achou@parktudor.org>
2019-02-22 21:18:55 -0500

    basicAI Connect4

--- src/BasicAIConnect4.java
+++ src/BasicAIConnect4.java
@@ -8,46 +8,49 @@ public class BasicAIConnect4 extends Player {
     private String moveToWin;
     private String moveToBlock;
     public BasicAIConnect4 (String name){
-        super(name); moveToWin ="none"; moveToBlock="none";
+        super(name);
     }
 
     public String getMove(Board board) {
-        String location = "";
+        ArrayList<String>b = board.getEmptyLocs();
+        for (int i = 0; i<b.size();i++){
+            board.placePiece(b.get(i), "R");
+            if (board.isWinner("R")){
+                board.retractPiece(b.get(i));
+                return b.get(i);
+            }
+            else
+                board.retractPiece(b.get(i));
+        }
+
+        for (int i = 0; i<b.size();i++){
+            board.placePiece(b.get(i), "Y");
+            if (board.isWinner("Y")){
+                board.retractPiece(b.get(i));
+                return b.get(i);
+            }
+            else
+                board.retractPiece(b.get(i));
+        }
+
         Random r = new Random();
-        ArrayList<String> nums = board.getEmptyLocs();
-
-        if (canWin(board) ){
-            location = moveToWin;
-            System.out.println("win");
-
-        }
-        else if(blockOpponent(board) ) {
-            location = moveToBlock;
-            System.out.println("block");
-        }
-        else{
-            int length = 7;
-            int rand = r.nextInt(length);
-            while (!board.get(1, rand).equals("-"))
-                rand = r.nextInt(length);
-            location = Integer.toString(rand+1);
-        }
-        return location;
+        int num = r.nextInt(b.size());
+        return b.get(num);
     }
 
-    private boolean canWin(Board gameBoard) {
+   /* private boolean canWin(Board gameBoard) {
         boolean answer = false;
-        for (int c = gameBoard.getCols()-1; c >= 0; c--) {
-            if (avaliableMoves(c, gameBoard)){
+        for (int c = gameBoard.getCols()-1; c > 0; c--) {
+           // if (avaliableMoves(c, gameBoard)){
                 String num = Integer.toString(c);
                 gameBoard.placePiece(num, "R");
                 if (gameBoard.isWinner("R")) {
                     moveToWin = num;
+                    c = -1;
                 }
                 gameBoard.retractPiece(num);
-            }
-            break;
-    }
+            //}
+        }
         if(!moveToWin.equals("none"))
             answer = true;
         return answer;
@@ -55,48 +58,17 @@ public class BasicAIConnect4 extends Player {
 
     public boolean blockOpponent(Board gameBoard) {
         boolean answer = false;
-        for (int c = gameBoard.getCols()-1; c >= 0; c--) {
-            if (avaliableMoves(c, gameBoard)) {
+        for (int c = gameBoard.getCols()-1; c > 0; c--) {
                 String num = Integer.toString(c);
                 gameBoard.placePiece(num, "Y");
                 if (gameBoard.isWinner("Y")) {
                     moveToBlock = num;
+                    c= -1;
                 }
                 gameBoard.retractPiece(num);
-            }
-            break;
         }
         if(!moveToBlock.equals("none"))
             answer = true;
         return answer;
-    }
-
-    public ArrayList<Integer> allAvaliableMoves (Board gameboard){
-        ArrayList<Integer> avaliable = new ArrayList<>();
-        for (int c = 0; c<gameboard.getCols();c++){
-            for (int r = gameboard.getRows()-1; r>0;r--){
-                if(gameboard.get(r, c).equals("-")){
-                    avaliable.add(r);
-                    r = 0;
-                }
-            }
-        }
-        System.out.println(avaliable);
-        return avaliable;
-    }
-
-    public boolean avaliableMoves (int c, Board gameboard){
-        boolean answer = false;
-        int num = 0;
-        for (int r = 1; r<gameboard.getRows();r++){
-            if (gameboard.get(r, c) == ("-"))
-                num++;
-        }
-
-        if (num == 0)
-            answer = false;
-        else
-            answer = true;
-        return answer;
-    }
+    }*/
 }
